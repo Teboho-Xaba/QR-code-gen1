2f7de526ad31743dd1ea7bfef2600df0be489b6f
Tebza <99969842+Teboho-Xaba@users.noreply.github.com>
2023-01-25 23:20:55 +0200

    qr-codeGen1

---
+++ script.js
@@ -0,0 +1,16 @@
+let imgBox = document.getElementById("img-box");
+let qrImg = document.getElementById("qrImage");
+let qrTxt = document.getElementById("qrText");
+
+function generateQr(){
+    if (qrTxt.value.length > 0){
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrTxt.value;
+    imgBox.classList.add("show-img");
+    }
+    else {
+        qrTxt.classList.add('error');
+        setTimeout(() =>{
+            qrTxt.classList.remove('error');
+        }, 1000);
+    }
+}
